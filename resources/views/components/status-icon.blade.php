17cd29a962f22b0e3d91cad332b5b41cf64d763e
AnandaAnugrahHandyanto <168060635+AnandaAnugrahHandyanto@users.noreply.github.com>
2026-05-11 22:53:03 +0700

    fix: close remaining status icon svg paths
    Co-Authored-By: AnandaAnugrahHandyanto <168060635+AnandaAnugrahHandyanto@users.noreply.github.com>

--- resources/views/components/status-icon.blade.php
+++ resources/views/components/status-icon.blade.php
@@ -9,13 +9,13 @@
 
     @case('izin')
         <svg {{ $attributes->class('h-4 w-4') }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V19a2 2 0 01-2 2" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
         </svg>
     @break
 
     @case('sakit')
         <svg {{ $attributes->class('h-4 w-4') }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
     @break
 
