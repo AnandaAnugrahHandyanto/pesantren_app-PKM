c98196fd2f6baf31ae49d2d660d8210f4185859c
AnandaAnugrahHandyanto <168060635+AnandaAnugrahHandyanto@users.noreply.github.com>
2026-05-11 22:52:43 +0700

    fix: restore closed svg paths for status icons
    Co-Authored-By: AnandaAnugrahHandyanto <168060635+AnandaAnugrahHandyanto@users.noreply.github.com>

--- resources/views/components/status-icon.blade.php
+++ resources/views/components/status-icon.blade.php
@@ -3,7 +3,7 @@
 @switch($status)
     @case('hadir')
         <svg {{ $attributes->class('h-4 w-4') }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
     @break
 
@@ -21,6 +21,6 @@
 
     @default
         <svg {{ $attributes->class('h-4 w-4') }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
 @endswitch
